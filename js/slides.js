ac21eb080a4a1523cb2d7a0f89badafc855e1254
Marcelo3duardo <fmarcelo3duardo@gmail.com>
2021-12-14 18:04:31 -0300

    erro no slick

---
+++ js/slides.js
@@ -0,0 +1,30 @@
+
+
+$(function () {
+
+
+    /* slides superior dando erro 
+    
+        $('.mosaico .mosaico--wraper ').slick({
+            centerMode:true,
+            slidesToShow:6,
+        });
+    */
+        /*
+    $('section.tratamentos .container-tartamento').slick({
+        centerMode:false,
+        slidesToShow:3,
+        arrows:false,
+        infinite:false
+
+    });
+    */
+})
+
+
+/*
+   toda vez que aplica o slide ele fica de canto
+   provavelmente eh o flex direction do prorpio fremework!
+   ** Adicionei um container mas pode ser retirado pois n faz diferenca 
+   porem vale a pena tentar um display block nele;
+*/
